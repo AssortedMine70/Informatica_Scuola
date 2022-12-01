76f8c0bde27478b118d18ed041a6e07810fc77d2
AssortedMine70 <50272777+AssortedMine70@users.noreply.github.com>
2022-12-01 12:52:54 +0100

    Add files via upload

--- 4AIN/Informatica/CarthesianPlane/src/CartesianPlane.java
+++ 4AIN/Informatica/CarthesianPlane/src/CartesianPlane.java
@@ -1,14 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-// Xf = W/(Xmax-Xmin) x Xm + W/2
+// Xf = W/(Xmax-Xmin) x Xm + W/21
 // Yf = -Ym[H/(Ymax-Ymin)] + H/2
 class Panel extends JPanel {
-    private final int XOffset; private final int YOffset;
     private final int DentsGap; private final int DentsSize;
     private final int ArrowSize;
-    public Panel(int XOffset, int YOffset, int DentsGap, int DentsSize, int ArrowSize) {
-        this.XOffset = XOffset;
-        this.YOffset = YOffset;
+    public Panel(int DentsGap, int DentsSize, int ArrowSize) {
         this.DentsGap = DentsGap;
         this.DentsSize = DentsSize;
         this.ArrowSize = ArrowSize;
@@ -21,21 +18,21 @@ class Panel extends JPanel {
     }
 
     private void drawAxis(Graphics g) {
-        g.drawLine(getWidth()/2 + XOffset, YOffset, getWidth()/2 + XOffset, getHeight() + YOffset);
-        g.drawLine(XOffset, getHeight()/2 + YOffset, getWidth() + XOffset, getHeight()/2 + YOffset);
+        g.drawLine(getWidth()/2, 0, getWidth()/2, getHeight());
+        g.drawLine(0, getHeight()/2, getWidth(), getHeight()/2);
     }
 
     private void drawArrows(Graphics g) {
-        g.fillPolygon(new int[] {getWidth(), getWidth()-ArrowSize, getWidth()-ArrowSize}, new int[] {getHeight()/2, getHeight()/2 + ArrowSize, getHeight()/2 - ArrowSize}, 3);
-        g.fillPolygon(new int[] {getWidth()/2, getWidth()/2+ArrowSize, getWidth()/2-ArrowSize}, new int[] {0, ArrowSize, ArrowSize}, 3);
+        g.fillPolygon(new int[] {getWidth(), getWidth() - ArrowSize, getWidth() - ArrowSize}, new int[] {getHeight() / 2, getHeight() / 2 + ArrowSize, getHeight() / 2 - ArrowSize}, 3);
+        g.fillPolygon(new int[] {getWidth() / 2, getWidth() / 2 + ArrowSize, getWidth() / 2 - ArrowSize}, new int[] {0, ArrowSize, ArrowSize}, 3);
     }
 
     private void drawDents(Graphics g) {
-        for (int i = 0; i < getWidth(); i+= DentsGap) {
+        for (int i = 0; i < Math.max(getWidth(), getHeight()); i+= DentsGap) {
             if(i < getWidth()-ArrowSize)
-                g.drawLine(i + XOffset, getHeight() / 2 - DentsSize + YOffset, i  + XOffset, getHeight() / 2 + DentsSize + YOffset);
+                g.drawLine(i, getHeight() / 2 - DentsSize, i, getHeight() / 2 + DentsSize);
             if(i > ArrowSize)
-            g.drawLine(getWidth() / 2 - DentsSize + XOffset, i  + YOffset, getWidth() / 2 + DentsSize + XOffset, i  + YOffset);
+                g.drawLine(getWidth() / 2 - DentsSize, i, getWidth() / 2 + DentsSize, i);
         }
     }
 }
@@ -46,7 +43,10 @@ public class CartesianPlane {
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new Panel(0, 0, 5, 1, 5);
+        JPanel panel = new Panel(5, 1, 5);
+        //panel.setDomainRange(xmin, xmax);
+        //panel.setImagesRange(ymin, ymax);
+        //panel.setFunction(Poly);
         panel.setBackground(Color.WHITE);
         frame.add(panel);
 
